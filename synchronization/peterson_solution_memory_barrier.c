37033661d770b142ac8199f2fd6d3901747b644b
knightchaser <agerio100@naver.com>
2024-10-24 22:00:49 +0900

    Implementation of Peterson's solution with memory barrier

---
+++ synchronization/peterson_solution_memory_barrier.c
@@ -0,0 +1,73 @@
+// synchronization/peterson_solution_memory_barrier.c
+// A Simple C implementation of the Peterson's solution, with memory barriers.
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+_Atomic bool flag[2] = {false, false};
+_Atomic int turn = 0;
+
+void criticalSection(int id) {
+    printf("Thread %d is in critical section\n", id);
+    sleep(1);
+    printf("Thread %d is out of critical section\n", id);
+}
+
+void nonCriticalSection(int id) {
+    printf("Thread %d is in non-critical section\n", id);
+    sleep(1);
+    printf("Thread %d is out of non-critical section\n", id);
+}
+
+void *thread0 (void *arg) {
+    for (int try = 0; try < 5; try++) {
+        // Entry section
+        atomic_store(&flag[0], true);
+        atomic_store(&turn, 1);
+        
+        // - No instruction reordering across the fence, to ensure modifying the flag and turn before the while loop
+        atomic_thread_fence(memory_order_seq_cst);
+
+        while (atomic_load(&flag[1]) && atomic_load(&turn) == 1)
+            ;
+
+        // Critical section
+        criticalSection(0);
+
+        // Exit section
+        atomic_store(&flag[0], false);
+
+        // Non-critical section
+        nonCriticalSection(0);
+    }
+
+    return NULL;
+}
+
+void *thread1 (void *arg) {
+    for (int try = 1; try < 5; try++) {
+        // Entry section
+        atomic_store(&flag[1], true);
+        atomic_store(&turn, 0);
+
+        // - No instruction reordering across the fence, to ensure modifying the flag and turn before the while loop
+        atomic_thread_fence(memory_order_seq_cst);
+
+        while (atomic_load(&flag[0]) && atomic_load(&turn) == 0)
+            ;
+
+        // Critical section
+        criticalSection(1);
+
+        // Exit section
+        atomic_store(&flag[1], false);
+
+        // Non-critical section
+        nonCriticalSection(1);
+    }
+
+    return NULL;
+}
